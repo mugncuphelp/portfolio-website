2de038bd2c3e983be1f6925b39ee8a252a8cf4eb
mugncuphelp <mugncuphelp@gmail.com>
2023-01-18 19:55:03 +0800

    add app title, image, and description

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import streamlit as st
-import csv
+import pandas
+import math
+
+
+def display_app(app_df, start_index):
+    if start_index < len(app_df):
+        st.title(app_df['title'][start_index])
+        st.image(f'images/{app_df["image"][start_index]}')
+        st.info(app_df['description'][start_index])
+        st.write(f'[Source Code]({app_df["url"][start_index]})')
 
 
 st.set_page_config(layout='wide')
@@ -16,6 +25,28 @@ with col2:
     
     '''
     st.write(content)
+
+content2 = '''
+Below you can find some apps that i have built in python. Feel free to contact me
+'''
+st.write(content2)
+
+start_index = -1
+
+df = pandas.read_csv('data.csv')
+for app_row in range(math.ceil(len(df)/2)):
+    
+    col_left ,col_mid,  col_right = st.columns([1.5, 0.2, 1.5])
+    with col_left:
+        start_index = start_index + 1
+        display_app(df,start_index)
+
+    with col_right:
+        start_index = start_index + 1
+        display_app(df,start_index)
+
+    st.text(" ")
+
 
 
 
