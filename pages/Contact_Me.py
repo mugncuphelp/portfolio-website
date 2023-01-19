40bcea68ce3b534a4edd5d79b1f9329fe1f9b473
mugncuphelp <mugncuphelp@gmail.com>
2023-01-20 07:46:33 +0800

    add form to contact owner with email

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,3 +1,19 @@
 import streamlit as st
+from send_email import send_email
 
 st.header('Contact Me')
+
+with st.form(key='user_send_email_form'):
+    user_email = st.text_input('Enter your email address', key='user_email')
+    raw_message = st.text_area('Enter your message', key='message')
+    message = f"""\
+Subject: Message from Portfolio Website from {user_email}
+
+From: {user_email}
+{raw_message}
+
+"""
+    submit_button = st.form_submit_button()
+    if submit_button:
+        send_email(message)
+        st.info("Your email was sent succesfully")
